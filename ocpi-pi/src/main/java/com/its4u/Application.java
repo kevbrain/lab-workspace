e05a4f24c3caea3ae1d6a7ca5e30a9cff512b4c8
kevbrain <44575942+kevbrain@users.noreply.github.com>
2021-08-20 10:36:35 +0200

    Update Application.java

--- ocpi-pi/src/main/java/com/its4u/Application.java
+++ ocpi-pi/src/main/java/com/its4u/Application.java
@@ -1,6 +1,6 @@
 package com.its4u;
 
-
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(Application.class, args);
-
-	}
-	
+		 SpringApplication app = new SpringApplication(Application.class);
+		 app.setBannerMode(Banner.Mode.CONSOLE);
+                 app.run();
+	}	
 }
